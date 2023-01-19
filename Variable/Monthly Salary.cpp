b9ad09cb27bd8847ebfe40dd2491d2e3c5d28ecb
MdAhaduzzamanKushal <kushal.tay@gmail.com>
2023-01-19 18:41:18 +0600

    Find montly salary from annual salary

---
+++ Variable/Monthly Salary.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float AnnualSalary = 55564.55;
+    float MonthlySalary = AnnualSalary / 12;
+
+    cout << "Monthly Salary :" <<MonthlySalary;
+
+    system("pause>0");
+
+}
